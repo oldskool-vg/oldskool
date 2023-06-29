5606fcea22f15ffeb7b1d9dcf4a10e729d7068e2
Rachel <rkmiller131@gmail.com>
2023-06-29 16:42:08 -0700

    Adding boiler plate files Java Backend

---
+++ src/main/java/com/cfvanguard/oldskool/OldskoolApplication.java
@@ -0,0 +1,13 @@
+package com.cfvanguard.oldskool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldskoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldskoolApplication.class, args);
+	}
+
+}
