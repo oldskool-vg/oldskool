8253025779f13868e3b265dd82875577f231a958
Naru Sadakuni <narusadakuni1@gmail.com>
2023-07-03 10:10:20 -0700

    framework complete for front end. rendering 3 cards based on mock data.

---
+++ src/main/ui/src/components/Cards.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import Card from './Card'
+import { CardData } from '../types';
+
+const mockData: CardData[] = [
+  {
+    _id: "momsSpaghettiHotnReady",
+    name: "Abyss Healer",
+    grade: "0",
+    cardType: "Trigger Unit",
+    power: 5000,
+    shield: 10000,
+    skill: "Boost",
+    flavor: "Not yet! I will not let you fall before we demolish the enemy!",
+    effect: "(You may only have up to four cards with 'HEAL' in a deck.)",
+    clan: "Shadow Paladin",
+    race: "Angel",
+    image: "https://cfv-card-images.s3.us-east-2.amazonaws.com/42-en.png",
+    sets: ["Booster Set 4: Eclipse of Illusionary Shadows"],
+    legality: "classic",
+  },
+  {
+    _id: "loseUrselfInTheMusicTheMoment",
+    name: "Armored Fairy, Shubiela",
+    grade: "3",
+    cardType: "",
+    power: 10000,
+    shield: 0,
+    skill: "Twin Drive!!",
+    flavor: "",
+    effect: "[AUTO](VC/RC):[Soul Blast (3)] When this unit's attack hits, if you have a «Nova Grappler» vanguard, you may pay the cost. If you do, draw a card.[AUTO](VC): When this unit is boosted by a «Nova Grappler», this unit gets [Power]+3000 until end of that battle.",
+    clan: "Nova Grappler",
+    race: "Battleroid",
+    image: "https://cfv-card-images.s3.us-east-2.amazonaws.com/375-en.png",
+    sets: [
+        "Booster Set 4: Eclipse of Illusionary Shadows",
+        "Extra Booster: Infinite Phantom Legion"
+    ],
+    legality: "classic",
+  },
+  {
+    _id: "catchTheseHands",
+    name: "Flash Shield, Iseult",
+    grade: "1",
+    cardType: "",
+    power: 6000,
+    shield: 0,
+    skill: "Boost",
+    flavor: "",
+    effect: "[CONT]:Sentinel (You may only have up to four cards with [CONT]:Sentinel in a deck.)[AUTO]:[Choose a «Royal Paladin» from your hand, and discard it] When this unit is placed on (GC), you may pay the cost. If you do, choose one of your «Royal Paladin» that is being attacked, and that unit cannot be hit until end of that battle.",
+    clan: "Royal Paladin",
+    race: "Human",
+    image: "https://cfv-card-images.s3.us-east-2.amazonaws.com/3642-en.png",
+    sets: [
+                "G Comic Booster 1: Vanguard & Deletor",
+                "Booster Set 1: Descent of the King of Knights"
+    ],
+    legality: "classic",
+  }
+]
+
+const Cards = () => {
+  const [cards, setCards] = useState<CardData[]>([]);
+
+  useEffect(() => {
+    setCards(mockData);
+  }, []);
+
+    return (
+      <div className='cards'>
+        {cards.map((card) => (
+          <Card key={card._id} card={card}/>
+      ))}
+      </div>
+    )
+  }
+
+export default Cards
---
+++ src/main/ui/src/types.ts
@@ -0,0 +1,17 @@
+export type CardData = {
+  _id: string;
+  name: string;
+  grade: string;
+  cardType: string;
+  power: number;
+  shield: number;
+  skill: string;
+  flavor: string;
+  effect: string;
+  clan: string;
+  race: string;
+  image: string;
+  sets: string[];
+  legality: string;
+};
+
