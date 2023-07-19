05c0d01a68f8f20cca80fa349ee182b18080c1a9
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-07-18 17:06:30 -0700

    Completed unstyled content for about page

--- src/main/ui/src/components/About/index.tsx
+++ src/main/ui/src/components/About/index.tsx
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function About() {
   return (
-    // Have 2 sections:
-    // What is Oldskool Vanguard?
-      // Put an explanation here
-    // Why Play Oldskool Vanguard?
-      // Put an explanation here
-
-    <div>About</div>
+    <>
+      <div>
+        <h2>What is Cardfight!! Vanguard?</h2>
+        <p>“Cardfight!! Vanguard” is a Trading Card Game (TCG) created by Akira Itou and published by Bushiroad. The game was first introduced in Japan on March, 2011 and then released internationally on December, 2011 with its first set, “Booster Set 1: Descent of the King of Knights.” The game's release coincided with a strong multimedia push as Bushiroad supported the franchise with a manga series, an anime television series, and a live action film.</p>
+        <p>In the CFVG TCG, players assume the role of “Cardfighters” who use decks of cards representing various characters from the science-fantasy world of Cray. Each player is represented by a powerful unit known as a “Vanguard” with the goal of defeating the opponent’s Vanguard by dealing them six points of damage through combat. The game gained widespread popularity through its bold visual designs and novel game mechanics.</p>
+        <p>Unfortunately, the game was unable to continue fueling its growth while retaining its playerbase. Though the game was previously celebrated for catering to diverse playstyles and deck builds, this changed with the release of "Booster Set 5: Awakening of Twin Blades" and the "Cross Ride" ability - a tremendously powerful mechanic that warped the game entirely around a few specific viable deck builds. Many players felt that the game had lost its strategic depth and variety as decks heavily reliant on Cross Ride cards overshadowed other playstyles, heavily contributing to the decline in Cardfight!! Vanguard's popularity.</p>
+      </div>
+      <div>
+        <h2>What is Oldskool Format?</h2>
+        <p>"Oldskool Format" is a historic Cardfight!! Vanguard format. Inspired by the Old School format in Magic: the Gathering TCG, Oldskool seeks to recreate the charm of the "early days" of CFVG.  Therefore, Oldskool seeks to represent the state of CFVG that existed prior to the release of Booster Set 5 through a limited card pool where all cards and sets released before Booster Set 5 are legal.</p>
+        <p>Note: Oldskool adheres to the North American release dates for its card pool. As Booster Set 5 was released February 22, 2013 in English, all sets released prior are legal in Oldskool. For more information, visit our Rules section.</p>
+      </div>
+      <div>
+        <h2>Why Play Oldskool?</h2>
+        <h4>If you love CFVG but don't enjoy the pace and mechanics (Stride, Imaginary Gifts, Overtriggers) of the Premium/V-Premium/Standard formats, Oldskool is for you!</h4>
+        <p>Trading Card Games experience powercreep, and CFVG is no exception. As a game advances through many years of powercreep, the iconic cards that once carried the game are left forgotten in shoeboxes and binders. In addition, as recent releases become increasingly complex in their design, keeping up with the game can become difficult. These are a few of the many reasons why Oldskool should be considered - Oldskool features a limited card pool that allows for iconic creatures from CFVG's history to be played at the prime of their power.</p>
+        <p>As Oldskool represents the state of CFVG that existed prior to the release of Booster Set 5 in North America, Oldskool is a space for tremendous player expression in deckbuilding and playstyle. Oldskool encompasses a period of time featuring everything from fan-favorites to obscure build-arounds, preserving a broad diversity of strategies. In addition, because Oldskool uses the North American product release schedule in determining its card pool, the Oldskool format offers room for innovation that would not be viable in a format dominated by Cross Rides.</p>
+        <p>Give Oldskool a try! Stand up, Vanguard!</p>
+      </div>
+    </>
   )
 }
 export default About;
