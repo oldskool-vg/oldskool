7ea3b6b05e0ec7639c3d8047750a5728cbbd4603
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-07-22 12:36:06 -0700

    Created filter data to create filter options for searching

---
+++ src/main/ui/src/components/CardDatabase/filterData.js
@@ -0,0 +1,43 @@
+const grade = ["0", "1", "2", "3"];
+
+const power = [3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000];
+
+const shield = [0, 5000, 10000];
+
+const clan = [
+  "Royal Paladin",
+  "Shadow Paladin",
+  "Gold Paladin",
+  "Oracle Think Tank",
+  "Angel Feather",
+  "Kagero",
+  "Narukami",
+  "Tachikaze",
+  "Nubatama",
+  "Bermuda Triangle",
+  "Granblue",
+  "Dark Irregulars",
+  "Spike Brothers",
+  "Pale Moon",
+  "Great Nature",
+  "Megacolony",
+  "Nova Grappler",
+  "Dimension Police",
+]
+
+const sets = [
+  "Booster Set 1: Descent of the King of Knights",
+  "Booster Set 2: Onslaught of Dragon Souls",
+  "Booster Set 3: Demonic Lord Invasion",
+  "Booster Set 4: Eclipse of Illusionary Shadows",
+  "Booster Set 6: Breaker of Limits",
+  "Booster Set 7: Rampage of the Beast King",
+  "Trial Deck 1: Blaster Blade",
+  "Trial Deck 2: Dragonic Overlord",
+  "Trial Deck 3: Golden Mechanical Soldier",
+  "Trial Deck 4: Maiden Princess of the Cherry Blossoms",
+  "Trial Deck 5: Slash of Silver Wolf",
+  "Trial Deck 6: Resonance of Thunder Dragon",
+  "Extra Booster: Banquet of Divas",
+  "Extra Booster: Cavalry of Black Steel",
+]
