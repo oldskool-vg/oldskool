b656c18d15bb43c2f60cd88c61b2afe05a3fe8a1
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-07-24 14:41:20 -0700

    Styled "about" page

--- src/main/ui/src/components/About/index.tsx
+++ src/main/ui/src/components/About/index.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-
 function About() {
   return (
-    <>
-      <div>
+    <div className="about">
+      <div className="paragraph-container">
         <h2>What is Cardfight!! Vanguard?</h2>
         <p>“Cardfight!! Vanguard” is a Trading Card Game (TCG) created by Akira Itou and published by Bushiroad. The game was first introduced in Japan on March, 2011 and then released internationally on December, 2011 with its first set, “Booster Set 1: Descent of the King of Knights.” The game's release coincided with a strong multimedia push as Bushiroad supported the franchise with a manga series, an anime television series, and a live action film.</p>
         <p>In the CFVG TCG, players assume the role of “Cardfighters” who use decks of cards representing various characters from the science-fantasy world of Cray. Each player is represented by a powerful unit known as a “Vanguard” with the goal of defeating the opponent’s Vanguard by dealing them six points of damage through combat. The game gained widespread popularity through its bold visual designs and novel game mechanics.</p>
         <p>Unfortunately, the game was unable to continue fueling its growth while retaining its playerbase. Though the game was previously celebrated for catering to diverse playstyles and deck builds, this changed with the release of "Booster Set 5: Awakening of Twin Blades," and with it, the "Cross Ride" ability - a tremendously powerful mechanic that warped the game entirely around a few specific viable deck builds. This lead to a significant decrease in deck variety and a decline in CFVG's popularity as strategies heavily reliant on Cross Ride cards overshadowed other playstyles.</p>
       </div>
-      <div>
+      <div className="paragraph-container">
         <h2>What is Oldskool Format?</h2>
         <p>"Oldskool Format" is a historic Cardfight!! Vanguard format. Inspired by the Old School format in Magic: the Gathering TCG, Oldskool seeks to recreate the charm of the "early days" of CFVG.  The Oldskool Format achieves this through a limited card pool where all cards and sets released before Booster Set 5 are legal for constructed play.</p>
         <p>Note: Oldskool adheres to the North American release dates for its card pool, which had a different release schedule than Japan. As Booster Set 5 was released February 22, 2013 in English, all sets released prior are legal in Oldskool - including Booster Sets 6 and 7. For more information, visit our Rules section.</p>
       </div>
-      <div>
+      <div className="paragraph-container">
         <h2>Why Play Oldskool?</h2>
         <h4>If you love CFVG but don't enjoy the pace and mechanics (Stride, Imaginary Gifts, Overtriggers) of the Premium/V-Premium/Standard formats, Oldskool is for you!</h4>
         <p>Trading Card Games experience powercreep, and CFVG is no exception. As a game advances through many years of powercreep, the iconic cards that once defined the game are left behind. In addition, as recent releases become increasingly complex in their design, keeping up with the game can become difficult. These are a few of the many reasons why Oldskool should be considered - Oldskool features a limited card pool that allows for iconic cards from CFVG's history to be played at the prime of their power.</p>
@@ -22,7 +20,7 @@ function About() {
         <p>In summary, Oldskool captures the core fundamentals of what makes Cardfight Vanguard worth playing. The card pool supports tremendous deck diversity while catering to every playstyle imaginable, and the format strikes a perfect balance between power level of the cards and the importance of skillfully maneuvering your strategy to a winning position. We hope you'll give Oldskool a try.</p>
         <p>"Stand Up, Vanguard!"</p>
       </div>
-    </>
+    </div>
   )
 }
 export default About;
