4e2f91856703984c81f6213cbef78d6e2e292b85
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-08-09 10:59:07 -0700

    Added error page

---
+++ src/main/ui/src/components/ErrorPage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>An invalid URL was requested. Please try again with a valid URL endpoint.</p>
+    </div>
+  );
+}
+export default ErrorPage;
