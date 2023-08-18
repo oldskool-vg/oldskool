4cd26a0d7214c9ade0c7669450df545d21a592ff
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-08-18 15:55:55 -0700

    Added a background image to the homepage

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,39 +1,43 @@
 import { useCard } from '../../App';
+import bg from '../assets/cray-bg.jpg';
 
 function HomePage() {
   const { card } = useCard();
 
   return (
-    <div className="home">
-      {
-      card ?
-        (
-          <div className="COTDContainer">
-            <h1>Card Of The Day:</h1>
-            <div className="COTD">
-              <img src={card.imageurlen} alt="Card Of The Day"/>
-              <div className="COTDInfo">
-                <div>
-                  <p>Name: { card.name }</p>
-                  <p>Grade: { card.grade }</p>
-                  <p>Power: { card.power }</p>
-                  <p>Shield: { card.shield }</p>
-                  <p>Effect: { card.effect }</p>
-                  <p>Flavor Text: { card.flavor }</p>
-                  <p>Clan: { card.clan }</p>
+    <div className="homeContainer">
+      <div className="home">
+        {
+        card ?
+          (
+            <div className="COTDContainer">
+              <div className="COTDHeader">
+                <h1>Card Of The Day: { card.name }</h1>
+              </div>
+              <div className="COTD">
+                <div className="cardContainer">
+                  <img src={card.imageurlen} alt="Card Of The Day"/>
+                </div>
+                <div className="COTDInfo">
+                  <div>
+                    <p>Name: { card.name }</p>
+                    <p>Grade: { card.grade }</p>
+                    <p>Power: { card.power }</p>
+                    <p>Shield: { card.shield }</p>
+                    <p>Flavor Text: { card.flavor }</p>
+                    <p>Effect: { card.effect }</p>
+                    <p>Clan: { card.clan }</p>
+                  </div>
                 </div>
               </div>
-            </div>
 
-          </div>
-        ) : (
-          <div>
-            <h1>Loading Card Of The Day...</h1>
-          </div>
-        )
-      }
-      <div className="disclaimerContainer">
-        <p className="legalDisclaimer">©bushiroad All Rights Reserved.</p>
+            </div>
+          ) : (
+            <div className="COTDContainer">
+              <h1>Loading Card Of The Day...</h1>
+            </div>
+          )
+        }
       </div>
     </div>
   )
