5af9280d59d4178d235020128ebbe2c20508aa71
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-08-21 11:09:14 -0700

    Deleted unnecessary code from /About

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { accordionData } from './accordionData';
 import Accordion from './Accordion';
-import Discord from '../Discord';
 import banner2 from '../assets/banner2.jpg';
 
 function About() {
@@ -9,10 +8,6 @@ function About() {
   return (
     <div className="about">
       <img src={banner2} className="bannerByItself" alt="banner1" />
-      {/* <div className="bannerContainer">
-        <img src={banner2} className="banner" alt="banner1" />
-        <Discord />
-      </div> */}
 
       <div className="accordion">{
         accordionData.map(({title, content}) => (
